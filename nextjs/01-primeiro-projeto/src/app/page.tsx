885b9c87f8aebf296cd5911c7cf2edcf57e5af59
kessygodoy <kessyon@gmail.com>
2025-03-17 17:06:26 -0300

    Estilizado com nextJs, Otimização de imagens

--- nextjs/01-primeiro-projeto/src/app/page.tsx
+++ nextjs/01-primeiro-projeto/src/app/page.tsx
@@ -1,95 +1,47 @@
+'use client'
+
+import { Card, Heading } from "@radix-ui/themes";
+import styles from "./page.module.scss";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <header className={styles.header}>
+        <nav>
+          <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+          </ul>
+        </nav>
+      </header>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <Heading as="h1">Olá, Next.js!</Heading>
+        <Card>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <ul>
+            <li>
+              <div>Via Lactea</div>
+              <Image src="/via-lactea.jpg" alt="Via Lactea" width={400} height={225} />
+            </li>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+            <li>
+              <div>Andromeda</div>
+              <Image
+                src="https://images-assets.nasa.gov/image/PIA04921/PIA04921~medium.jpg"
+                width={400}
+                height={225}
+                alt="Andromeda"
+                style={{ objectFit: 'cover' }}
+              />
+            </li>
+            
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </Card>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
