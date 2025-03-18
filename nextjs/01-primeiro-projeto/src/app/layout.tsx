73ae076c28369e411f20475effe6d0b80320312a
kessygodoy <kessyon@gmail.com>
2025-03-18 15:01:23 -0300

    incluido fontes locais e remotas

--- nextjs/01-primeiro-projeto/src/app/layout.tsx
+++ nextjs/01-primeiro-projeto/src/app/layout.tsx
@@ -1,5 +1,20 @@
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
+import localFont from "next/font/local";
+import "./globals.css"
+import { Quicksand } from "next/font/google";
+
+const robotoCondensed = localFont({
+  src: "./fonts/Roboto_Condensed.ttf",
+  variable: "--font-roboto-condensed",
+  weight: "400 700",
+});
+
+const quicksandFont = Quicksand({
+  variable: "--font-quicksand",
+  weight: "300",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>
+      <body className={`${robotoCondensed.variable} ${quicksandFont.variable}`} >
         <Theme appearance='dark'>
           {children}
         </Theme>
