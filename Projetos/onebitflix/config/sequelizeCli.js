51438ea7c26ce92bda455591d07ed7ff47fe7ed5
kessygodoy <kessyon@gmail.com>
2025-03-26 19:36:00 -0300

    pasta para aulas de typescript criada e movido o projeto onebitflix para pasta projetos

---
+++ Projetos/onebitflix/config/sequelizeCli.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        username: "postgres",
+        password: "k112233k",
+        host: "localhost",
+        dialect: "postgres",
+        database: "onebitflix_development"
+    },
+}
