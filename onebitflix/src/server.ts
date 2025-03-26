34b17f21ac8cdc1d7e10e6b8f274d39cddb54d9a
kessygodoy <kessyon@gmail.com>
2025-03-26 16:34:52 -0300

    configurado o adminjs

--- onebitflix/src/server.ts
+++ onebitflix/src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import { sequelize } from './database';
+import { adminJs, adminJsRouter } from './adminjs';
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,5 +14,6 @@ app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
     console.log("DB connection successful");
   })
+
   console.log(`Server is running on port ${PORT}`);
 });
