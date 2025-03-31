6aa2934469f4e4abab579f0b4acabe34f3b87bce
kessygodoy <kessyon@gmail.com>
2025-03-31 09:07:34 -0300

    Relacionamentos entre models

--- Projetos/onebitflix/src/adminjs/resources/index.ts
+++ Projetos/onebitflix/src/adminjs/resources/index.ts
@@ -1,10 +1,16 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { Course } from "../../models/Course";
+import { courseResourceOptions } from "./course";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions
   }
 ]
